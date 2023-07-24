c6c3e39d85d97f929b4a0d740b0fe5a56ba122b9
melly <kurabiyecanavari773@gmail.com>
2023-07-24 16:32:39 +0300

    code

---
+++ code.js
@@ -0,0 +1,22 @@
+
+function convertDemicalToBinary(number){
+    let binary = "";
+    while(true){
+        binary+=(number%2).toString();
+        number = Math.floor(number/2);
+        if(number == 1){
+            binary+=1;
+            break;
+        }
+    }
+    let result = reverse(binary);
+    alert("sonuç : " + result);
+}
+
+function reverse(binary){
+    let reverseBinary = "";
+    for(let i = binary.length-1 ; i>=0; i--){
+        reverseBinary += binary.charAt(i);
+    }
+    return reverseBinary;
+}
